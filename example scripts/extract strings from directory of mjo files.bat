fe5c6f96267f83c442202bbe33e3b1a276e07d69
Jaiden Baker <jaidencolebaker@gmail.com>
2021-11-19 14:20:59 +1300

    Fixed typo in example scripts

--- example scripts/extract strings from directory of mjo files.bat
+++ example scripts/extract strings from directory of mjo files.bat
@@ -4,6 +4,6 @@ Echo You'll need to extract the mjo files manually with arc_conv before running 
 Echo Check the Readme at https://github.com/Jonodonozym/MajiroStringEditor for more details
 Echo Also delete any empty files in the output directory; they are not scenario scripts
 
-if not exist scenario-extracted mkdir scenario-txt
+if not exist scenario-extracted mkdir scenario-extracted
 for %%i in (scenario\*.mjo) do MajiroStringEditor --extract "%%i" "scenario-extracted\%%~nxi"
 pause
